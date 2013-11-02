c3667434eb211b0f745872793723dcffe6e304d4
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-11-02 19:58:57 +0200

    Use less and uglify-js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
     jshint: {
       options: {
         jshintrc: ".jshintrc"
@@ -15,17 +16,63 @@ module.exports = function(grunt) {
         src: "relaxed-json.js"
       },
       webcli: {
-        src: "web/cli.js",
+        src: "web.js",
         options: {
           browser: true,
           node: false,
         },
       },
     },
+    uglify: {
+      core: {
+        src: "relaxed-json.js",
+        dest: "relaxed-json.min.js",
+        options: {
+          sourceMap: "relaxed-json.min.js.map",
+          report: "min",
+        },
+      },
+      web: {
+        banner: "/* <%= pkg.name %> - <%= pkg.version %> */",
+        src: [
+          "components/codemirror.js",
+          "components/cm-mode-javascript.js",
+          "components/jquery-2.0.3.js",
+          "relaxed-json.js",
+          "web.js",
+        ],
+        dest: "web.min.js",
+        options: {
+          sourceMap: "web.min.js.map",
+          report: "min",
+        },
+      }
+    },
+    less: {
+      web: {
+        src: [
+          "components/codemirror.css",
+          "web.less",
+        ],
+        dest: "web.min.css",
+        options: {
+          report: "min",
+          compress: true,
+          strictMath: true,
+          strictImports: true,
+          strictUnits: true,
+          syncImport: true,
+          sourceMap: true,
+          sourceMapFilename: "web.min.css.map",
+        }
+      },
+    }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-less");
 
   // Default task.
   grunt.registerTask("default", ["jshint"]);
