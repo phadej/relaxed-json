b1e53dc6eaa9df18e30d323b6790afa891465a92
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-11-09 17:30:56 +0200

    Revert "Remove web stuff"
    This reverts commit 52214298ff9ee8ed830e79146e0e078e3022d845.

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,12 +47,58 @@ module.exports = function(grunt) {
           report: "min",
         },
       },
+      web: {
+        src: [
+          "components/codemirror.js",
+          "components/cm-mode-javascript.js",
+          "components/jquery-2.0.3.js",
+          "components/underscore.js",
+          "components/jquery.cookie.js",
+          "relaxed-json.js",
+          "web.js",
+        ],
+        dest: "web.min.js",
+        options: {
+          sourceMap: "web.min.js.map",
+          report: "min",
+        },
+      }
+    },
+    less: {
+      web: {
+        src: [
+          "web.less",
+        ],
+        dest: "web.min.css",
+        options: {
+          report: "min",
+          compress: true,
+          strictMath: true,
+          strictImports: true,
+          strictUnits: true,
+          syncImport: true,
+          sourceMap: true,
+          sourceMapFilename: "web.min.css.map",
+        }
+      },
+    },
+    watch: {
+      less: {
+        files: "<%= less.web.src %>",
+        tasks: ["less"],
+      },
+      uglify: {
+        files: "<%= uglify.web.src %>",
+        tasks: ["uglify:web"],
+      },
     },
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-simple-mocha");
 
   // Default task.
